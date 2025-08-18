571242c706e005857e9c8b1ddb136072d68b4120
Alan-Pinheiro <alanpinheiro5@hotmail.com>
2025-08-18 19:10:34 -0300

    correção dos campos com vírgula ao final

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-let numeroSecreto = criarNumeroAleatorio()
-let tentativas = 1
+let numeroSecreto = criarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
@@ -17,22 +17,22 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
 
-  let chute = document.querySelector('input').value
+  let chute = document.querySelector('input').value;
 
-  let quantasTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
+  let quantasTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
 
   if(chute == numeroSecreto) {
-    exibirTextoNaTela('h1', 'Acertou!')
-    exibirTextoNaTela('p', `Você descobriu o número secreto, com ${tentativas} ${quantasTentativas}!`)
+    exibirTextoNaTela('h1', 'Acertou!');
+    exibirTextoNaTela('p', `Você descobriu o número secreto, com ${tentativas} ${quantasTentativas}!`);
   } else {
     if (chute > numeroSecreto) {
-      exibirTextoNaTela('p', 'O número secreto é menor que o chute.')
+      exibirTextoNaTela('p', 'O número secreto é menor que o chute.');
     } else {
-      exibirTextoNaTela('p', 'O número secreto é maior que o chute.')
+      exibirTextoNaTela('p', 'O número secreto é maior que o chute.');
     }
   } tentativas++
 }
 
 function criarNumeroAleatorio () {
-  return parseInt(Math.random() * 10 + 1)
+  return parseInt(Math.random() * 10 + 1);
 }
